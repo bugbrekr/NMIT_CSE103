e8a61bbf41119d9439d02d42c01fc49161268e62
BugBrekr <me@bugbrekr.dev>
2025-09-25 00:23:15 +0530

    finished LA1/p5.c

--- LA1/p5.c
+++ LA1/p5.c
@@ -12,7 +12,7 @@ Date: 24/09/25
 
 void main() {
 	int n;
-	printf("Enter size of array you would like to input: ");
+	printf("Enter the size of array you would like to input: ");
 	scanf("%d", &n);
 
 	if (n < 1) { // guard clause
@@ -27,5 +27,28 @@ void main() {
     }
 
     int even_arr[n], odd_arr[n];
+    int e_i=0, o_i=0; // hold indices for even_arr and odd_arr
+    for (int i=0; i<n; i++) {
+        if (arr[i]%2==0) { // check if even
+            even_arr[e_i] = arr[i]; // append to even_arr
+            e_i++;
+        } else {
+            odd_arr[o_i] = arr[i]; // append to odd_arr
+            o_i++;
+        }
+    }
+
+    printf("Even numbers: ");
+    int *e_pt = even_arr; // get pointer of first element in even_arr
+    for (int i=0; i<e_i; i++) {
+        printf("%d ", *(e_pt+i)); // get value of any element in even_arr using pointer
+    }
+    printf("\n");
     
+    printf("Odd numbers: ");
+    int *o_pt = odd_arr; // get pointer of first element in odd_arr
+    for (int i=0; i<o_i; i++) {
+        printf("%d ", *(o_pt+i)); // get value of any element in odd_arr using pointer
+    }
+    printf("\n");
 }
